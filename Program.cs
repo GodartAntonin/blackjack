d8fb69b99d1dd469b78e4888800e93fcee1435c9
Antonin <godartantonin@gmail.com>
2023-11-02 21:26:56 +0100

    Half of second part

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace Blackjack
             int croupierCard;
             Random random = new Random();
 
+            // First turn
             Console.WriteLine("Le Croupier pioche une carte");
             croupierCard = random.Next(1, 11);
             if (croupierCard == 1 && croupierScore <= 11)
@@ -22,11 +23,12 @@ namespace Blackjack
             {
                 croupierScore += 1;
             }
-            
+
             Console.WriteLine("A votre tour de piocher une carte");
             playerCard = random.Next(1, 11);
-            Console.WriteLine("Votre carte est "+playerCard);
-            if (playerCard==1) {
+            Console.WriteLine("Votre carte est " + playerCard);
+            if (playerCard == 1)
+            {
                 string answer = "";
                 while (answer != "1" && answer != "11")
                 {
@@ -36,7 +38,56 @@ namespace Blackjack
                 playerScore += Int32.Parse(answer);
                 Console.WriteLine("Vous avez donc un total de " + playerScore + " points");
             }
-           
+            else
+            {
+                playerScore += playerCard;
+            }
+
+            // Second turn
+            Console.WriteLine("Le croupier repioche ");
+            croupierCard = random.Next(1, 11);
+            if (croupierCard == 1 && croupierScore <= 11)
+            {
+                croupierScore += 11;
+            }
+            else
+            {
+                croupierScore += 1;
+            }
+            if (croupierScore > 22)
+            {
+                Console.WriteLine("Le Croupier a sauté vous avez gagné! Voulez-vous rejouer? Appuyer sur O pour Oui et N pour Non");
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+            Console.WriteLine("A votre tour de piocher une carte");
+            playerCard = random.Next(1, 11);
+            Console.WriteLine("Votre carte est " + playerCard);
+            if (playerCard == 1)
+            {
+                string answer = "";
+                while (answer != "1" && answer != "11")
+                {
+                    Console.WriteLine("Vous avez pioché un as, souhaitez-vous 1 ou 11 points ? ");
+                    answer = Console.ReadLine() ?? "";
+                }
+                playerScore += Int32.Parse(answer);
+                Console.WriteLine("Vous avez donc un total de " + playerScore + " points");
+            }
+            else
+            {
+                playerScore += playerCard;
+            }
+            if (playerScore >= 22)
+            {
+                Console.WriteLine("Vous avez sauté, pas de chance. Voulez-Vous recommencer ? Si oui appuyer sur O et sinon appuyer sur N");
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
         }
     }
 }
